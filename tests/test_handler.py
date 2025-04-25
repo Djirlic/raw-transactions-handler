60384c935a3f27b62fa93c3a7a43d032ead187fe
Manuel Djirlic <freelance@djirlic.com>
2025-04-25 23:03:46 +0200

    test: fix broken test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,7 +1,18 @@
-from handler import main
+import handler.handler as handler
 
 
-def test_handle():
-    dummy_event = {}
+def test_handle(mocker):
+    dummy_event = {
+        "Records": [{"s3": {"bucket": {"name": "dummy-bucket"}, "object": {"key": "dummy.csv"}}}]
+    }
     dummy_context = object()
-    assert main(dummy_event, dummy_context) is None
+    mock_download = mocker.Mock(return_value="/tmp/dummy.csv")
+    mock_transform = mocker.Mock()
+    mocker.patch("handler.storage.download_file_from_s3", mock_download)
+    mocker.patch("handler.transform.transform_csv_to_parquet", mock_transform)
+
+    result = handler.handle_event(dummy_event, dummy_context)
+
+    mock_download.assert_called_once_with("dummy-bucket", "dummy.csv")
+    mock_transform.assert_called_once_with("/tmp/dummy.csv", "/tmp/dummy.csv.parquet")
+    assert result is None
