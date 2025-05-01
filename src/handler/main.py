629feed29a700f81c2f18dddd89f5f6ee1768511
Manuel Djirlic <freelance@djirlic.com>
2025-05-02 01:55:29 +0200

    feat: add handling logic back in

--- src/handler/main.py
+++ src/handler/main.py
@@ -1,14 +1,11 @@
 import logging
-import os
-import sys
+
+from handler.handler import handle_event
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def main(event, context):
-    logger.info("===== DEBUG START =====")
-    logger.info(f"sys.path = {sys.path}")
-    logger.info(f"/opt/python contents = {os.listdir('/opt/python')}")
-    logger.info("===== DEBUG END =====")
-    return {"status": "debug printed"}
+def main(event, context) -> None:
+    logger.info("Starting main function")
+    return handle_event(event, context)
