230cc9989af323837c788604d28b9202a7ff4e55
Manuel Djirlic <freelance@djirlic.com>
2025-05-02 01:27:32 +0200

    chore: add advanced logging

--- src/handler/main.py
+++ src/handler/main.py
@@ -1,8 +1,14 @@
 import logging
+import os
 import sys
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-def main(event, context) -> None:
-    logger.info("sys.path =", sys.path)
+def main(event, context):
+    logger.info("===== DEBUG START =====")
+    logger.info(f"sys.path = {sys.path}")
+    logger.info(f"/opt/python contents = {os.listdir('/opt/python')}")
+    logger.info("===== DEBUG END =====")
+    return {"status": "debug printed"}
