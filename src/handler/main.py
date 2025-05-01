9e0d7c85a8eaf479bba13d2071f26374d5a3e5bb
Manuel Djirlic <freelance@djirlic.com>
2025-05-02 01:22:22 +0200

    chore: remove import of handler for testing

--- src/handler/main.py
+++ src/handler/main.py
@@ -1,11 +1,8 @@
 import logging
 import sys
 
-from handler.handler import handle_event
-
 logger = logging.getLogger(__name__)
 
 
 def main(event, context) -> None:
     logger.info("sys.path =", sys.path)
-    return handle_event(event, context)
