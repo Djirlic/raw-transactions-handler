e3d8e7df1f11c0589d91f4f81ae741e6ea4504a5
Manuel Djirlic <freelance@djirlic.com>
2025-05-06 23:58:05 +0200

    test: fix broken test and ensure orchestration of handler does work

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,3 +1,5 @@
+import polars as pl
+
 import handler.handler as handler
 
 
@@ -7,12 +9,18 @@ def test_handle(mocker):
     }
     dummy_context = object()
     mock_download = mocker.Mock(return_value="/tmp/dummy.csv")
+
+    df = pl.DataFrame({"col1": [1, 2], "col2": ["a", "b"]})
+    mock_validate = mocker.Mock(return_value=df)
     mock_transform = mocker.Mock()
+
     mocker.patch("handler.storage.download_file_from_s3", mock_download)
-    mocker.patch("handler.transform.transform_csv_to_parquet", mock_transform)
+    mocker.patch("handler.validator.load_and_validate_csv", mock_validate)
+    mocker.patch("handler.transform.transform_dataframe_to_parquet", mock_transform)
 
     result = handler.handle_event(dummy_event, dummy_context)
 
     mock_download.assert_called_once_with("dummy-bucket", "dummy.csv")
-    mock_transform.assert_called_once_with("/tmp/dummy.csv", "/tmp/dummy.csv.parquet")
+    mock_validate.assert_called_once_with("/tmp/dummy.csv")
+    mock_transform.assert_called_once_with(df, "/tmp/dummy.csv.parquet")
     assert result is None
